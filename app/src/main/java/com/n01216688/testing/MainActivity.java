5609278841d66ec2d3259635ed9b001a397fbde2
benjaminle9x <54958544+benjaminle9x@users.noreply.github.com>
2019-10-09 20:11:05 -0400

    Added Menu Toolbar to Setting Screen

--- app/src/main/java/com/n01216688/testing/MainActivity.java
+++ app/src/main/java/com/n01216688/testing/MainActivity.java
@@ -1,10 +1,14 @@
 package com.n01216688.testing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -33,8 +37,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.setting:
+                openSettingPage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void openCustomerPage() {
         Intent intent = new Intent(this, CustomerPage.class);
         startActivity(intent);
+    }
+
+    public void openSettingPage(){
+        Intent intent1 = new Intent(this,SettingScreen.class);
+        startActivity(intent1);
     }
 }
