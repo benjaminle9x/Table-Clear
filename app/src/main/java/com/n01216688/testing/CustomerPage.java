176d3183b86224d4fd6915df79f235a39b3d4c3c
benjaminle9x <54958544+benjaminle9x@users.noreply.github.com>
2019-11-08 14:07:55 -0500

    Insert and Retrieve Data from Database successfully implemented.

--- app/src/main/java/com/n01216688/testing/CustomerPage.java
+++ app/src/main/java/com/n01216688/testing/CustomerPage.java
@@ -8,34 +8,51 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CustomerPage extends AppCompatActivity {
 
     ViewFlipper v_flipper;
-    Button logout;
+    Button logout, save, viewProf;
+    FirebaseDatabase database;
+    DatabaseReference myref;
+    DataStructure mData;
+    EditText name, phone, address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_page);
 
         getSupportActionBar().setTitle("Customer Page");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         int images[] = {R.drawable.pizza,R.drawable.delivery,R.drawable.walking};
 
         v_flipper = findViewById(R.id.v_flipper);
         logout = findViewById(R.id.logout);
+        save = findViewById(R.id.save);
+        name = findViewById(R.id.inName);
+        phone = findViewById(R.id.inPhone);
+        address = findViewById(R.id.inAdd);
+        viewProf = findViewById(R.id.viewProf);
+
+        getDatabase();
 
         //For loop
         for(int i = 0; i < images.length; i++){
@@ -46,6 +63,20 @@ public class CustomerPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeData(name.getText(), phone.getText(), address.getText());
+            }
+        });
+
+        viewProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCheckProfile();
             }
         });
 
@@ -123,6 +154,48 @@ public class CustomerPage extends AppCompatActivity {
                 });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
 
+    private void getDatabase() {
+        database = FirebaseDatabase.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        String path = "userdata/" + mAuth.getUid();
+        myref = database.getReference(path);
+    }
+
+    private DataStructure createData(Editable name, Editable phone, Editable address){
+        Long time = System.currentTimeMillis()/1000;
+        String timestamp = time.toString();
+        return new DataStructure(String.valueOf(name),
+                        String.valueOf(phone),
+                        String.valueOf(address),
+                        timestamp);
+    }
+
+    private void writeData(Editable name, Editable phone, Editable address) {
+        DataStructure mData = createData(name,phone,address);
+        myref.push().setValue(mData).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "Changes saved", Toast.LENGTH_LONG).show();
+                openRetrievingPage();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Saving failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void openRetrievingPage() {
+        Intent i3 = new Intent(CustomerPage.this,checkProfile.class);
+        startActivity(i3);
+    }
+
+    public void openCheckProfile() {
+        Intent i4 = new Intent(this, checkProfile.class);
+        startActivity(i4);
     }
 }
